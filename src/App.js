50817d805bca688c2b2c7e68520b4e1b87c470f2
degandt <tyler.degand@civicplus.com>
2021-06-03 15:18:52 -0500

    added Google Analytics

--- src/App.js
+++ src/App.js
@@ -2,19 +2,11 @@ import React from 'react';
 import Home from './components/home';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import ReactGA from 'react-ga';
-
-
 
 const App = () => {
-
-  ReactGA.initialize('UA-175982896-1'); // add your tracking id here.
-  ReactGA.pageview(window.location.pathname + window.location.search);
-
   React.useEffect(() => {
 		AOS.init();
 	}, []);
-
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/normalize.css';
 import './styles/index.css';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-175982896-1') // add your tracking id here.
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 ReactDOM.render(
   <React.StrictMode>
